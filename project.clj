b63efe2a23f1f5a27514d19734cad85ce4bad47c
Hitesh Jasani <hitesh@grokken.com>
2014-11-21 20:21:05 -0500

    Bump to v0.1.2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject org.jasani/common "0.1.1"
+(defproject org.jasani/common "0.1.2"
   :description "Commonly used Clojure routines"
   :url "https://github.com/hiteshjasani/clj-common"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
+
